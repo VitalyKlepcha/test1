71145657020cecfdd0f3141318b209979f1d11e8
VitalyKlepcha <vital.klepcha@mail.ru>
2021-04-08 10:35:11 +0300

    laba4

--- src/main/java/com/example/test1/controller/MainController.java
+++ src/main/java/com/example/test1/controller/MainController.java
@@ -4,7 +4,6 @@ import com.example.test1.exceptions.InputDataException;
 import com.example.test1.service.FileWorker;
 import com.example.test1.parametres.entityParametres;
 import com.example.test1.service.SortListService;
-import com.example.test1.service.impl.SortListServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +19,7 @@ import java.util.ArrayList;
 @Controller
 public class MainController {
     private static final String DATABASE = "D:\\test1\\database.txt";
+    private static final String ERRORDATABASE = "D:\\test1\\errorDatabase.txt";
     private FileWorker fileWorker;
     private SortListService sortListService;
     private entityParametres entityParametres;
@@ -56,13 +56,16 @@ public class MainController {
             entityParametres = new entityParametres(text1, text2);
             entityParametres.Reverse();
             list.add(entityParametres);
-            for(entityParametres el : list)
-                System.out.println(el.getFirstLine() + el.getSecondLine());
+            Thread listOutputThread = new Thread(new ListOutputRunner(list));
+            listOutputThread.start();
             list = sortListService.Sort(list);
             fileWorker.write(DATABASE, list);
         }
         catch(InputDataException ex){
             model.addAttribute("error", ex.getMessage());
+            fileWorker.write(ERRORDATABASE, ex.getMessage());
+            Thread errorOutputThread = new Thread(new ErrorsOutputRunner(fileWorker));
+            errorOutputThread.start();
         }
 
         model.addAttribute("leftInput", entityParametres.getFirstLine());
@@ -73,5 +76,34 @@ public class MainController {
     public String database(Model model){
         model.addAttribute("data", fileWorker.read(DATABASE));
         return "database";
+    }
+}
+
+class ListOutputRunner implements Runnable{
+    private ArrayList<entityParametres> list;
+    ListOutputRunner(ArrayList<entityParametres> list){
+        this.list = list;
+    }
+
+    @Override
+    public void run(){
+        for(entityParametres el : list)
+            System.out.println(el.getFirstLine() + el.getSecondLine());
+    }
+}
+
+class ErrorsOutputRunner implements Runnable{
+    private static final String ERRORDATABASE = "D:\\test1\\errorDatabase.txt";
+    private FileWorker fileWorker;
+    private static final Logger log = LogManager.getLogger();
+
+    public ErrorsOutputRunner(FileWorker fileWorker) {
+        this.fileWorker = fileWorker;
+    }
+
+    @Override
+    public void run(){
+        String message = fileWorker.read(ERRORDATABASE);
+        log.info(message);
     }
 }
